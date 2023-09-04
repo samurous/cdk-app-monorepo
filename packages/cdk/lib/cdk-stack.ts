69d0a22f907640956f1a017e18c7563057e66c1b
samuel <samuel@kiteswarms.com>
2023-09-04 23:20:39 +0200

    use @demo/config in cdk

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { urls } from '@demo/config';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
@@ -7,7 +8,7 @@ export class CdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
+    console.log(urls.GOOGLE);
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
